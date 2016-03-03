ee2dd160169b64556d53bdcfbacf67acd2698bef
dvp236 <dvp236@nyu.edu>
2016-03-02 23:11:50 -0500

    *Modified Methods

--- src/linkedlist/problems/FindElement.java
+++ src/linkedlist/problems/FindElement.java
@@ -2,16 +2,26 @@ package linkedlist.problems;
 
 public class FindElement {
 	
+	
+	//Gives the index of Element
+	//Time Complexity O(n)
 	public int getIndexOfElement(Node head,int data){
 		int index=0;
 		Node current = head;
+		
+		//loop over the linked list until the data matches the node data
 		while(current.getNext()!=null){
+			if(current.getData() == data){
+				return index;
+			}
 			index++;
 			current = current.getNext();
 		}
-		return index;
+		return -1;
 	}
 	
+	//Returns the NthtoLastNode
+	//For Ex: getNthToLast(head,0) would return last Node
 	public Node getNthToLast(Node head, int n){
 		Node first = head;
 		Node second = head;
