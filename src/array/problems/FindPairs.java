ad0e3d6aa86b9a1462528d0d674804b62422c2ed
dvp236 <dvp236@nyu.edu>
2016-03-20 20:26:19 -0400

    *modified find pairs problem

--- src/array/problems/FindPairs.java
+++ src/array/problems/FindPairs.java
@@ -2,28 +2,46 @@ package array.problems;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
-
+/**
+ * 
+ * @author dharmik
+ * Find pairs of element from an array a where sum of pairs 
+ * equals to given target number.
+ */
 public class FindPairs {
-
-	public static void main(String[] args) {
-		int[] a = {7,1,2,3,4,5,6};
+	
+	//Complexity O(nlog(n))
+	public static HashSet<List<Integer>> getPairs(int[] a, int target){
+		//sorting with cost nlog(n)
 		Arrays.sort(a);
-		List<int[]> res = new ArrayList<>();
-		System.out.println(Arrays.asList(a).indexOf(1));
-		int target = 9;
+		HashSet<List<Integer>> resultSet = new HashSet<>();
+		//n loop iteration.
 		for(int i=0;i<a.length;i++){
 			int temp = target-a[i];
 			
+			//Binary Search complexity log(n)
 			if(Arrays.binarySearch(a, temp) != -1){
-				
-				int[] t ={a[i],temp};
-				res.add(t);
+				final int min = a[i] < temp ? a[i] : temp;
+				final int max = a[i] == min ? temp : a[i];
+				//using list to ensure immutability instead of int array
+				//immutability required for hashset to work properly
+				List<Integer> pair = Arrays.asList(min,max);
+				resultSet.add(pair);
 			}
 				
 		}
-		for(int[] c : res){
-			System.out.println(Arrays.toString(c));
+		return resultSet;
+	}
+	
+	public static void main(String[] args) {
+		int[] a = {7,1,2,3,4,5,6};
+		Arrays.sort(a);
+		HashSet<List<Integer>> res= getPairs(a, 9);
+		
+		for(List<Integer> c : res){
+			System.out.println(Arrays.toString(c.toArray()));
 		}
 		
 	}
