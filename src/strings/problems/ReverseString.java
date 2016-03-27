e3d2f98dcdef89010249be73bc3f095efa0d6851
dvp236 <dvp236@nyu.edu>
2016-03-26 22:06:34 -0400

    *added more methods

--- src/strings/problems/ReverseString.java
+++ src/strings/problems/ReverseString.java
@@ -1,16 +1,27 @@
 package strings.problems;
 
+/**
+ * 
+ * @author dharmik
+ * Write different solutions to reverse string
+ */
 public class ReverseString {
 	
+	//Returns reverse string. Uses java's StringBuilder class. 
 	public String reverseSB(String st){
 		StringBuilder sb =  new StringBuilder(st);
 		sb = sb.reverse();
 		return sb.toString();
 	}
 	
+	//custom method to reverse string.
+	//time complexity O(n).
 	public String myreverse(String st){
+	
 		char[] res = st.toCharArray();
 		int left = 0, right = st.length() -1 ;
+		
+		//this loop will execute n/2  times.
 		while(left <= right){
 			char temp = res[left];
 			res[left] = res[right]; 
@@ -18,7 +29,20 @@ public class ReverseString {
 			left++;
 			right--;
 		}
+		
 		return new String(res);
+	}
+	
+	//Method to reverse an integer
+	public int reverseInt(int n){
+		int result = 0;
+		
+		while(n > 0){
+			result = result*10 + n%10;
+			n = n/10;
+		}
+		
+		return result;
 	}
 	
 	public static void main(String[] args){
@@ -26,6 +50,6 @@ public class ReverseString {
 		ReverseString r = new ReverseString();
 		System.out.println(r.reverseSB(st));
 		System.out.println(r.myreverse(st));
-		
+		System.out.println(r.reverseInt(1234));
 	}
 }
