ca0b7543f184f98962e1f7c4c9b121dad73db46d
dvp236 <dvp236@nyu.edu>
2016-04-11 23:43:39 -0400

    *updated minimalTree problem

--- src/tree/problems/MinimalTree.java
+++ src/tree/problems/MinimalTree.java
@@ -1,5 +1,26 @@
 package tree.problems;
 
+/**
+ * 
+ * @author dharmik
+ * Generate a Binary Tree with minimum height 
+ * from the given sorted array of unique element
+ * 
+ */
 public class MinimalTree {
-
+	public TreeNode generateMinimalTree(int[] a){
+		TreeNode root = generateMinimalTree(a,0,a.length-1);
+		return root;
+	}
+	
+	private static TreeNode generateMinimalTree(int[] a, int start, int end){
+		if(start >= end){
+			return null;
+		}
+		int mid = (start + end)/2;
+		TreeNode root = new TreeNode(a[mid]);
+		root.left = generateMinimalTree(a, start, mid-1);
+		root.right = generateMinimalTree(a, mid+1, end);
+		return root;
+	}
 }
---
+++ src/tree/problems/TreeNode.java
@@ -0,0 +1,22 @@
+package tree.problems;
+
+
+/**
+ * 
+ * @author dharmik
+ * TreeNode 
+ * 
+ */
+
+public class TreeNode {
+	
+	TreeNode left,right;
+	int data;
+	
+	public TreeNode(int data){
+		this.data = data;
+		left = null;
+		right = null;
+	}
+	
+}
