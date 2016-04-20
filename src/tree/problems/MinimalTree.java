80b13117947c87e49f305ba12d54803f3d6fcd1e
dvp236 <dvp236@nyu.edu>
2016-04-19 23:20:48 -0400

    *Added comments ->Minimal Tree

--- src/tree/problems/MinimalTree.java
+++ src/tree/problems/MinimalTree.java
@@ -8,17 +8,25 @@ package tree.problems;
  * 
  */
 public class MinimalTree {
+	
+	//A good strategy can be to pick the middle element of array and
+	//make it root element, since the array is sorted.
+	
 	public TreeNode generateMinimalTree(int[] a){
 		TreeNode root = generateMinimalTree(a,0,a.length-1);
 		return root;
 	}
 	
+	//Creating recursive function to generate a tree
 	private static TreeNode generateMinimalTree(int[] a, int start, int end){
 		if(start >= end){
 			return null;
 		}
+		//Create root node from the mid element
 		int mid = (start + end)/2;
 		TreeNode root = new TreeNode(a[mid]);
+		
+		//generate left subtree with values less than mid element and right subtree vice versa
 		root.left = generateMinimalTree(a, start, mid-1);
 		root.right = generateMinimalTree(a, mid+1, end);
 		return root;
