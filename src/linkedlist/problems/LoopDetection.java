7a52228bef06ead83ea32a98ba4d617b1af985ad
dvp236 <dvp236@nyu.edu>
2016-05-19 16:26:04 -0400

    *Added Linked List Problem

---
+++ src/linkedlist/problems/LoopDetection.java
@@ -0,0 +1,13 @@
+package linkedlist.problems;
+
+/**
+ * 
+ * @author dharmik patel
+ * Detect if the Linked List has a loop.
+ * If it has a loop than return intersecting node
+ * if it has no loop than return null
+ *
+ */
+public class LoopDetection {
+
+}
