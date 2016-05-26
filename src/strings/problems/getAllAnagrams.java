67cf446cc6ff4f9d2a538257e627c7024042b2bd
dvp236 <dvp236@nyu.edu>
2016-05-26 04:47:32 -0400

    *updated anagrams

--- src/strings/problems/getAllAnagrams.java
+++ src/strings/problems/getAllAnagrams.java
@@ -1,8 +1,10 @@
 package strings.problems;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class getAllAnagrams {
+	
 	public static ArrayList<String> getAnagrams(String s){
 		ArrayList<String> result = new ArrayList<String>(2*s.length());
 		helper(result,"",s);
@@ -11,6 +13,7 @@ public class getAllAnagrams {
 	//helper(r,"",abc) -> helper(r,a,bc),helper(r,b,ac),helper(r,c,ab)
 	//helper(r,a,bc) -> helper(r,ab,c), helper(r,ac,b)
 	//helper(r,ab,c) -> helper(r,abc,"")-> base case.
+	//Time complexity - O(2^n+1)
 	private static void helper(ArrayList<String> result, String pre, String s){
 		if(s.length() == 0){
 			result.add(pre);
@@ -22,15 +25,43 @@ public class getAllAnagrams {
 		}
 		
 	}
+	
+	//Just another solution.
+	public static void getAnagrams2(String s){
+		char[] a = new char[s.length()];
+		for(int i=0;i<s.length();i++){
+			a[i] = s.charAt(i);
+		}
+		helper2(a,s.length());
+	}
+	
+	private static void helper2(char[] a, int n){
+		if(n==1){
+			System.out.println(Arrays.toString(a));
+			return;
+		}
+		else{
+			for(int i=0;i<n;i++){
+				swap(a,i,n-1);
+				helper2(a,n-1);
+				swap(a,i,n-1);
+			}
+		}
+	}
+	private static void swap(char[] a,int i,int j){
+		char temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+	
 	public static void main(String[] args){
 		String s = "abc";
-		ArrayList<String> list;
-		//System.out.println(s.substring(0,2));
+		/*ArrayList<String> list;
 		list = getAnagrams(s);
 		for(String a : list){
 			//if(a.length() == s.length())
 			System.out.println(a);
-		}
-		
+		}*/
+		//getAnagrams2(s);
 	}
 }
